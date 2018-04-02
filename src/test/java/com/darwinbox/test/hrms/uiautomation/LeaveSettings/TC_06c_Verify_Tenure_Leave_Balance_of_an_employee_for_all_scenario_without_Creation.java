bd600a7305a723e1cb4b3daf4f2d189894feefe1
Shikhar Srivastava <shikhar.s@darwinbox.io>
2018-04-02 13:28:34 +0530

    Tenure Self Run without creation

--- src/test/java/com/darwinbox/test/hrms/uiautomation/LeaveSettings/TC_06c_Verify_Tenure_Leave_Balance_of_an_employee_for_all_scenario_without_Creation.java
+++ src/test/java/com/darwinbox/test/hrms/uiautomation/LeaveSettings/TC_06c_Verify_Tenure_Leave_Balance_of_an_employee_for_all_scenario_without_Creation.java
@@ -24,7 +24,7 @@ import com.darwinbox.test.hrms.uiautomation.Utility.UtilityHelper;
 import com.darwinbox.test.hrms.uiautomation.helper.TestBase.TestBase;
 import com.darwinbox.test.hrms.uiautomation.helper.Wait.WaitHelper;
 
-public class TC_06b_Verify_Tenure_Leave_Balance_of_an_employee_for_all_scenario_without_Probation extends TestBase {
+public class TC_06c_Verify_Tenure_Leave_Balance_of_an_employee_for_all_scenario_without_Creation extends TestBase {
 
 	HomePage homepage;
 	LoginPage loginpage;
@@ -37,15 +37,15 @@ public class TC_06b_Verify_Tenure_Leave_Balance_of_an_employee_for_all_scenario_
 	UtilityHelper objUtil;
 	CommonActionClass commonAction;
 	
-	private static final Logger log = Logger.getLogger(TC_06b_Verify_Tenure_Leave_Balance_of_an_employee_for_all_scenario_without_Probation.class);
+	private static final Logger log = Logger.getLogger(TC_06c_Verify_Tenure_Leave_Balance_of_an_employee_for_all_scenario_without_Creation.class);
 
 	@BeforeClass
 	public void setup() throws Exception {
-		ExcelReader.setFilenameAndSheetName("Tenure_Leave_Scenarios.xlsx", "All_Without_Probation");
-//		WriteResultToExcel = UtilityHelper.getProperty("config", "Write.Result.to.excel");
-//		if(WriteResultToExcel.equalsIgnoreCase("Yes")) {
-//			ExcelWriter.copyExportFileToResultsDir();					
-//		}
+		ExcelReader.setFilenameAndSheetName("Leave_Scenarios_Without_Creation.xlsx", "Tenure_without_Creation");
+		WriteResultToExcel = UtilityHelper.getProperty("config", "Write.Result.to.excel");
+		if(WriteResultToExcel.equalsIgnoreCase("Yes")) {
+			ExcelWriter.copyExportFileToResultsDir();					
+		}
 	}
 
 	@BeforeMethod
@@ -70,15 +70,7 @@ public class TC_06b_Verify_Tenure_Leave_Balance_of_an_employee_for_all_scenario_
 		Assert.assertTrue(leavesAction.setEmployeeID(UtilityHelper.getProperty("config","Employee.id")), "Employee ID is set successfully to test");
 		Assert.assertTrue(loginpage.loginToApplication(), "User Loggin to Application as Admin");
 		Assert.assertTrue(commonAction.changeApplicationAccessMode("Admin"), "Application access changed to Admin mode");
-		Assert.assertTrue(homepage.clickUserProfileIconAdmin(), "Click on Settings link");		
-		Assert.assertTrue(rightMenuOption.clickSidebarSettings(), "Click on Settings link");
-		Assert.assertTrue(commonSettings.clickLeaves(), "Click on Leaves link");		
-		Assert.assertTrue(leavesAction.deleteLeaveTypeIfAlreadyPresent(), "Leave Type is presnt are deleted successfully");
-		Assert.assertTrue(leaveSettings.clickCreateLeavePolicies(), "Clicked on Create Leave Policies link");
-		Assert.assertTrue(createManageLeaves.selectGroupCompanyDropdown(0), "Select Group Company");				
-		Assert.assertTrue(leavesAction.createLeaveTypeWithMentionedScenarios(), "Leaves type with mentioned scenarios is created");		
-		Assert.assertTrue(leavesAction.setCreditOnTenureBasisLeaveScenario(), "Leaves type with mentioned scenarios is created");						
-		Assert.assertTrue(createManageLeaves.clickCreateLeavePolicySaveButton(), "Click on Create Leave Policy Save Button");
 		Assert.assertTrue(leavesAction.verifyEmployeeTenureBasedLeaveBalanceForFourEdgeDays(), "Leave Balance for whole leave cycle calculated successfully") ;
+
 	}
 }
