5d6ac26a6b771081f02bc91f0e1fa2bf833b65d0
Harsha <harsha.konakanchi@darwinbox.io>
2019-08-07 09:52:53 +0530

    Update failures

--- services/src/main/java/com/darwinbox/attendance/services/settings/ReportsDashboardServices.java
+++ services/src/main/java/com/darwinbox/attendance/services/settings/ReportsDashboardServices.java
@@ -277,16 +277,15 @@ public class ReportsDashboardServices extends Services {
         log.info("Response: " + response);
         JSONObject obj = new JSONObject(response);
         Reporter("Coloumn is : " + obj.getJSONArray("cols").getString(14), "INFO");
-<<<<<<< HEAD
 
         Reporter("Report value : " + obj.getJSONArray("update").getJSONArray(0).toList().get(14), "INFO");
 
-=======
- for(int i=0;i<=25;i++){
-        Reporter("Report value : " + obj.getJSONArray("update").getJSONArray(i).toList().get(14), "INFO");
-}
->>>>>>> Attendance Reports
+         for(int i=0;i<=25;i++){
+                Reporter("Report value : " + obj.getJSONArray("update").getJSONArray(i).toList().get(14), "INFO");
+        }
+
     }
+
     public void getReportDatewiseAttendanceStatus(Reports reports) {
 
         String url = getData("@@url") + "/reports/ShowRoster";
