183d1352876c5b9a54c3c0d972b55b0674cda52c
sande <sandeep.d@darwinbox.io>
2019-10-10 14:36:00 +0530

    Grade Object

--- services/src/main/java/com/darwinbox/core/employee/objects/Grade.java
+++ services/src/main/java/com/darwinbox/core/employee/objects/Grade.java
@@ -1,5 +1,58 @@
 package com.darwinbox.core.employee.objects;
 
+import com.darwinbox.core.services.BandServices;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Grade {
     private String id;
+
+    public String getGradeName() {
+        return gradeName;
+    }
+
+    public void setGradeName(String gradeName) {
+        this.gradeName = gradeName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBandName() {
+        return bandName;
+    }
+
+    public void setBandName(String bandName) {
+        this.bandName = bandName;
+    }
+
+    private String gradeName;
+    private String description;
+    private String bandName;
+
+
+
+    public Map<String,String> toMap(){
+
+        Map<String,String> data = new HashMap<>();
+
+        data.put("UserGrade[grade_name]",getBandName());
+
+        if(getDescription()!=null)
+            data.put("UserGrade[descriptions]", getDescription());
+
+
+        String bandId=new BandServices().getBands().get(getBandName());
+
+        data.put("UserGrade[band_name]",bandId);
+
+        return data;
+    }
+
 }
