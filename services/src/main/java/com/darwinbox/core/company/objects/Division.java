6072bd908e2b2c86131384d838af45c26bc38cfd
sande <sandeep.d@darwinbox.io>
2019-10-31 15:14:29 +0530

    //Division,GroupComapnyFiles

--- services/src/main/java/com/darwinbox/core/company/objects/Division.java
+++ services/src/main/java/com/darwinbox/core/company/objects/Division.java
@@ -1,8 +1,72 @@
 package com.darwinbox.core.company.objects;
 
+import com.darwinbox.core.employee.objects.BusinessUnit;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Division {
+
+
     private String id;
 
+    private String divisionName;
+    private List<String> selectBussinessUnitsID;
+    private String costCentreID;
 
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDivisionName() {
+        return divisionName;
+    }
+
+    public void setDivisionName(String divisionName) {
+        this.divisionName = divisionName;
+    }
+
+    public List<String> getSelectBussinessUnitsID() {
+        return selectBussinessUnitsID;
+    }
+
+    public void setSelectBussinessUnitsID(List<String> selectBussinessUnitsID) {
+        this.selectBussinessUnitsID = selectBussinessUnitsID;
+    }
+
+    public String getCostCentreID() {
+        return costCentreID;
+    }
+
+    public void setCostCentreID(String costCentreID) {
+        this.costCentreID = costCentreID;
+    }
+
+    public List<NameValuePair> toMap(){
+        List<NameValuePair> map =new ArrayList<>();
+
+        map.add(new BasicNameValuePair("TenantDivision[division_name]",getDivisionName()));
+
+        if (selectBussinessUnitsID != null) {
+            for (String businessUnitID : selectBussinessUnitsID) {
+                map.add(new BasicNameValuePair("TenantDivision[business_units][]",businessUnitID));
+            }
+        }
+
+        if(costCentreID!=null)
+        {
+            map.add(new BasicNameValuePair("TenantDivision[cost_center]",costCentreID));
+        }
+
+        return  map;
+    }
 
 }
